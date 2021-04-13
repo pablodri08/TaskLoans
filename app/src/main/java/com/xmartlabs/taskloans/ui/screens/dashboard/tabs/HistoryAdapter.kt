bdcee3abe7ab3e428f59310934102bbdacc0cf88
PabloR <pablotomas_1@hotmail.com>
2021-04-13 10:12:33 -0300

    Card Date added

--- app/src/main/java/com/xmartlabs/taskloans/ui/screens/dashboard/tabs/HistoryAdapter.kt
+++ app/src/main/java/com/xmartlabs/taskloans/ui/screens/dashboard/tabs/HistoryAdapter.kt
@@ -9,9 +9,16 @@ import com.xmartlabs.swissknife.core.extensions.layoutInflater
 import com.xmartlabs.taskloans.R
 import com.xmartlabs.taskloans.data.model.EntryUserRole
 import com.xmartlabs.taskloans.databinding.ListItemHistoryBinding
-import kotlinx.android.synthetic.main.list_item_history.view.*
+import java.util.Calendar
+import java.util.Date
+import java.util.GregorianCalendar
+import java.util.Locale
 
 class HistoryAdapter : ListAdapter<EntryUserRole, HistoryAdapter.HistoryHolder>(HistoryAdapter.DiffCallback()) {
+  companion object {
+    const val MONTH = 2
+    const val DAY = 0
+  }
 
   private class DiffCallback : DiffUtil.ItemCallback<EntryUserRole>() {
     override fun areItemsTheSame(
@@ -35,6 +42,7 @@ class HistoryAdapter : ListAdapter<EntryUserRole, HistoryAdapter.HistoryHolder>(
       private val itemBinding: ListItemHistoryBinding,
   ) : RecyclerView.ViewHolder(itemBinding.root) {
     fun bind(data: EntryUserRole) {
+      setCardDate(data.entryDate)
       if (data.role == HistoryFragment.UserRole.RECIPIENT) {
         val yellowTransfer = R.drawable.yellow_transfer
         val yellowWash = R.drawable.yellow_wash
@@ -44,13 +52,24 @@ class HistoryAdapter : ListAdapter<EntryUserRole, HistoryAdapter.HistoryHolder>(
         val violetWash = R.drawable.violet_wash
         setImages(violetTransfer, violetWash)
       }
-      itemBinding.historyCardLayout.history_card_text_name.text = data.name
+      itemBinding.historyCardTextName.text = data.name
+    }
+
+    private fun setCardDate(date: Date?) {
+      if (date != null) {
+        val cal = GregorianCalendar()
+        cal.time = date
+        val day = cal.get(DAY)
+        val month = cal.getDisplayName(MONTH, Calendar.SHORT, Locale.US)
+        val cardDate = "$day $month"
+        itemBinding.dateHistoryCard.text = cardDate
+      }
     }
 
     private fun setImages(imageTransfer: Int, imageWash: Int) {
-      itemBinding.historyCardLayout.history_card_image2.setBackgroundResource(imageTransfer)
-      itemBinding.historyCardLayout.history_card_image3.setBackgroundResource(imageWash)
-      itemBinding.historyCardLayout.history_card_image4.setBackgroundResource(imageTransfer)
+      itemBinding.historyCardImage2.setBackgroundResource(imageTransfer)
+      itemBinding.historyCardImage3.setBackgroundResource(imageWash)
+      itemBinding.historyCardImage4.setBackgroundResource(imageTransfer)
     }
   }
 }
